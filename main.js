3f43916893b7a36fe77bf137e18229f0ffc40f5a
ÅbbeLåbbe <Aarupnicolai@hotmail.com>
2022-02-22 11:37:58 +0100

    lavet burgermenu

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-SVGComponentTransferFunctionElement
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+/* Denne funktion gør at burgemenuen forsvinder når man klikker på en af siderne. */
+document.querySelectorAll("nav.link").forEach(n => n. 
+    addEventListener ("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active")
+    }))
